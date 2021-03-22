3eeec6f38623ed644ccab58c68b31b7718594376
ariq <ariqnvls@student.telkomuniversity.ac.id>
2021-03-22 09:59:43 +0700

    Mempelajari method onCreate dan Logging

--- AboutMeDataBinding-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ AboutMeDataBinding-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.example.android.aboutme
 
@@ -37,11 +23,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("MainActivity", "onCreate Called")
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart Called")
+    }
+
 
     /**
      * Click handler for the Done button.
